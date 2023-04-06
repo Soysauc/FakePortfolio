af860bb6918298a0069f0174b8573804d48cd081
Jeremy M Rivera <fangman09@gmail.com>
2023-04-06 18:20:02 -0400

    ready for submission

--- src/components/shared/AppFooterCopyright.jsx
+++ src/components/shared/AppFooterCopyright.jsx
@@ -1,26 +1,20 @@
 function AppFooterCopyright() {
-	return (
-		<div className="font-general-regular flex justify-center items-center text-center">
-			<div className="text-lg text-ternary-dark dark:text-ternary-light">
-				&copy; {new Date().getFullYear()}
-				<a
-					href="https://github.com/realstoman/react-tailwindcss-portfolio"
-					target="__blank"
-					className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
-				>
-					React & Tailwind CSS Portfolio
-				</a>
-				.
-				<a
-					href="https://stoman.me"
-					target="__blank"
-					className="text-secondary-dark dark:text-secondary-light font-medium uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
-				>
-					Stoman
-				</a>
-			</div>
-		</div>
-	);
+  return (
+    <div className="font-general-regular flex justify-center items-center text-center">
+      <div className="text-lg text-ternary-dark dark:text-ternary-light">
+        &copy; {new Date().getFullYear()}
+        <h2
+          target="__blank"
+          className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
+        >
+          Senior Capstone Portfolio
+        </h2>
+        <h2 className="text-secondary-dark dark:text-secondary-light font-medium uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500">
+          Jeremy Rivera
+        </h2>
+      </div>
+    </div>
+  );
 }
 
 export default AppFooterCopyright;
--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,10 +1,10 @@
 export const aboutMeData = [
-	{
-		id: 1,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
-	},
-	{
-		id: 2,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
-	},
+  {
+    id: 1,
+    bio: "An experienced freelance software engineer who has a knack for languages, programming and human, learning, and living. As a JavaScript developer who primarily uses React he is up to date with the relevant best practices and paradigms required to become and stay attuned to his craft.",
+  },
+  {
+    id: 2,
+    bio: " His education not only includes a technical STEM education that readied him for proficiency in his tech stack (MERN) but also prepared him with the tools necessary to learn other technologies re- lated to true mastery in his field. To round out his studies he is finishing a Bachelors degree in Communications, to explore not only the technical side of communication but also the social side.",
+  },
 ];
